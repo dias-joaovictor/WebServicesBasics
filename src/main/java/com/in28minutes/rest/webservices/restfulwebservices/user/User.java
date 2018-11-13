4c190135ff2e9736883cc364cf30c0062a6afd3b
silverfoxjv <kvj1610@hotmail.com>
2018-11-12 23:46:27 -0200

    Implementation of SWAGGER2 UI

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -5,15 +5,21 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description="All details about user. ")
 public class  User {
 
 	private Integer id;
 
 	
 	@Size(min=2, message="Name should have at least 2 characters")
+	@ApiModelProperty(notes="The person's name")
 	private String name;
 
 	@Past
+	@ApiModelProperty(notes="Birthdate in the past")
 	private Date birthDate;
 
 	public User() {
